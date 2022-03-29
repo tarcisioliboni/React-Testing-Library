05cb2044789b437a8da04c01f1f0f1710cf78fc4
Tarcisio Liboni <tarcisioliboni@pop-os.localdomain>
2022-03-29 18:13:04 -0300

    Request 03 OK

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import React from 'react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testando  Pokedex', () => {
+  test('Verificando informações Pokédex', () => {
+    const { customHistory } = renderWithRouter(<App />);
+
+    customHistory.push('/favorites');
+
+    const textAbout = screen.getByText(/No favorite pokemon found/i);
+    expect(textAbout).toBeInTheDocument();
+  });
+});
