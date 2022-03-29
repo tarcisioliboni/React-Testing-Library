bd410d889b492fbbd1e65b7fb2cb8229d0b18de8
Tarcisio Liboni <tarcisioliboni@pop-os.localdomain>
2022-03-29 18:32:41 -0300

    Request 04 OK

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,27 @@
-test('', () => {});
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import React from 'react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testando  links', () => {
+  test('Testanto Not Found para rota inexistente', async () => {
+    const { customHistory } = renderWithRouter(<App />);
+
+    customHistory.push('/xablau');
+
+    const titleNotFound = await screen.findByRole(
+      'heading', { level: 2, name: /Page requested not found/i },
+    );
+    expect(titleNotFound).toBeInTheDocument();
+  });
+
+  test('Teste se página mostra a imagem', () => {
+    const { customHistory } = renderWithRouter(<App />);
+
+    customHistory.push('/xablau');
+
+    const image = screen.getAllByRole('img');
+    expect(image[1]).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
